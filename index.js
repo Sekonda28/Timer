23aa2ba2c109d67e735d00fdceff97a8dee75686
Matt Caswell <matt.caswell@hotmail.com>
2021-09-24 14:01:42 +0200

    create class component

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hi there!")
+class Timer {
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+
+    }
+    start(){
+        this.importantMethodToCall();
+    }
+    importantMethodToCall(){
+        console.log('IMPORTANT THING');
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton)
+timer.start()
