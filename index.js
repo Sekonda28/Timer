d738d08ccd31a9449762272c5ff21928a7613d99
Matt Caswell <matt.caswell@hotmail.com>
2021-09-27 21:16:50 +0200

    finish timer class and add to separate file

--- index.js
+++ index.js
@@ -1,46 +1,3 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton, callbacks) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-        if (callbacks) {
-            this.onStart = callbacks.onStart;
-        }
-
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-
-    }
-    start = () => {
-
-        if (this.onStart) {
-            this.onStart();
-        }
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);
-    }
-
-    pause = () => {
-        clearInterval(this.interval);
-    }
-    tick = () => {
-        if (this.timeRemaining <= 0) {
-            this.pause()
-        } else {
-            this.timeRemaining = this.timeRemaining - 1;
-        }
-    }
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
@@ -50,9 +7,10 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
         console.log('Timer started');
     },
     onTick() {
-
+        console.log('Timer just ticked down');
     },
     onComplete() {
+        console.log('Timer is completed');
 
     }
 })
